3218364ee0528e1f9607a54b931111e882546a9a
Bruno Vicente <brunnovicente@gmail.com>
2024-12-16 22:09:13 -0300

    Enviando dados do cliente para o servidor

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import handlebars from 'express-handlebars'
 import Handlebars from 'handlebars'
+import bodyParser from 'body-parser'
 import { allowInsecurePrototypeAccess } from '@handlebars/allow-prototype-access'
 
 /*CONFIGURAÇÃO DA PASTA ESTATICA*/
@@ -16,6 +17,9 @@ app.engine('handlebars', handlebars.engine({
     handlebars: allowInsecurePrototypeAccess(Handlebars)
 }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 /**ROTAS DO SISTEMA */
 app.get('/', function(req, res){
@@ -30,6 +34,21 @@ app.get('/', function(req, res){
 
 app.get('/contato', function (req, res){
     res.render('admin/contato')
+})
+
+app.get('/cadastro', function(req, res){
+    res.render('produto/cadastro')
+})
+
+app.post('/cadastro', function(req, res){
+    var produto = {
+        descricao: req.body.descricao,
+        estoque: req.body.estoque,
+        preco: req.body.preco,
+        status: 1,
+        foto: '/img/semfoto.png'
+    }
+    res.render('produto/detalhe', {produto})
 })
 
 app.listen(3000, ()=> console.log('Servidor truando em http://localhost:3000'))
